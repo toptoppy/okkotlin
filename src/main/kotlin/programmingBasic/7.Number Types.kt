87429fd2089e03ddfa63d18a8db4dc369638adec
TopToppy Kananek <toptoppy@odds.team>
2025-01-23 22:32:43 +0700

    Add Number Types

---
+++ src/main/kotlin/programmingBasic/7.Number Types.kt
@@ -0,0 +1,26 @@
+package toptoppy.kotlin.training.programmingBasic
+
+fun main() {
+    val millionInt: Int = 1_000_000
+    val millionLong: Long = 1_000_000L
+    val millionLong2: Long = 1000000
+    println(millionInt)
+    println(millionLong)
+    println(millionLong2)
+
+    val weight = 65.5
+    val height = 1.69
+    println(weight)
+    println(height)
+
+    println(Int.MAX_VALUE)
+    println(Long.MAX_VALUE)
+    println(Double.MAX_VALUE)
+
+    val result = 4 + 5 * 6
+    println(result)
+
+    val result2 = ( 4 + 5 ) * 6
+    println(result2)
+
+}
